8461beca5d7448790b4d78bad9633db3ef175f7c
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-13 18:05:41 +0200

    Add project and base OOP class.

---
+++ DEXCource/OOP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEXCource
+{
+    class OOP
+    {
+    }
+    public abstract class Scientist
+    {
+        public Scientist(string Name, int IQ)
+        {
+            this.Name = Name;
+            this.IQ = IQ;
+        }
+        private string Name { get; }
+        private int IQ { get; }
+        public virtual void Introduse()
+        {
+            Console.WriteLine("Привет, меня зовут {0} и мой IQ равен {1}", Name, IQ);
+        }
+        public virtual void SaySomething()
+        {
+            Console.WriteLine("Если оно зеленое или дергается – это биология." +
+                "Если воняет – это химия." +
+                "Если не работает – это физика." +
+                "Если непонятно – это математика." +
+                "Если это бессмысленно – это либо экономика, либо психология.");
+        }
+        public abstract void DoSomething();
+    }
+}
---
+++ DEXCource/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DEXCource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, its a education project, run Unit Tests to see it!");
+            Console.ReadLine();
+        }
+    }
+}
