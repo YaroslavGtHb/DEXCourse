d66c787a77459bb175257f786e4c7ad545e9eb60
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-14 21:18:01 +0200

    Fix.

--- DEXCourceTests/LINQ.cs
+++ DEXCourceTests/LINQ.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace DEXCource
 {
     class LINQ
     {
-        
+        public void LINQTest()
+        {
+            var cats = BirdsGenerate(100);
+        }
+        public Collection<Cat> BirdsGenerate(int BirdsCount)
+        {
+            string[] birdNames = new string[8]
+            {
+                "Матроскин", "Гарфилд", "Рыжий", "Мурзик", "Снежок", "Полосатый", "Мемасик", "Том"
+            };
+            var random = new Random();
+
+            var generatedCats = new Collection<Cat>();
+            for (int i = 0; i < 100; i++)
+            {
+                generatedCats.Add(new Cat(generatedCats.Count, birdNames[random.Next(0, 7)], random.Next(1, 48), random.Next(100) < 50 ? true : false, random.NextDouble()));
+            }
+            return generatedCats;
+        }
     }
     public class Cat
     {
@@ -19,10 +38,10 @@ namespace DEXCource
             this.isMale = isMale;
             this.weight = weight;
         }
-        int id { get; }
-        string name { get; set; }
-        int age { get; set; }
-        bool isMale { get; }
-        double weight { get; set; }
+        public int id { get; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public bool isMale { get; }
+        public double weight { get; set; }
     }
 }
