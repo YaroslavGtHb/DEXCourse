e0032b241f14dd0afd41ae61aa7969efba01c78a
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-14 16:38:33 +0200

    Add ADD method to IEnumerableIEnumerator.

--- DEXCourceTests/IEnumerableIEnumerator.cs
+++ DEXCourceTests/IEnumerableIEnumerator.cs
@@ -6,36 +6,58 @@ namespace DEXCource
 {
     class IEnumerableIEnumerator
     {
+        public void IEnumerableIEnumeratorTest()
+        {
+            var zoo = new ZOOArray();
+            zoo.Add("Лев");
+            zoo.Add("Гепард");
+            zoo.Add("Барс");
+            zoo.Add("Алкаш");
+
+            foreach(string animal in zoo)
+            {
+                Console.WriteLine("В нашем зоопарке есть:");
+                Console.WriteLine(animal);
+            }
+        }
     }
 
-    public class ZOO
+    public class ZOOArray
     {
-        string[] Animals = { "Lion", "Mokney" };
+        string[] animals = new string[128];
+        public void Add(string Animal)
+        {
+            if (animals.Length > 128)
+            {
+                Array.Resize(ref animals, animals.Length + 128);
+            }
+            animals[animals.Length + 1] = Animal;
+        }
         public IEnumerator<string> GetEnumerator()
         {
-            return new ZOOEnumerator(Animals);
+            return new ZOOEnumerator(animals);
         }
         public class ZOOEnumerator : IEnumerator<string>
         {
-            string[] Animals;
+            string[] animals;
             public int Position { get; set; }
             public ZOOEnumerator(string[] days)
             {
-                Animals = Animals;
+                animals = animals;
             }
             public string Current
             {
                 get
                 {
-                    if (Position == -1 || Position >= Animals.Length)
+                    if (Position == -1 || Position >= animals.Length)
                         throw new InvalidOperationException();
-                    return Animals[Position];
+                    return animals[Position];
                 }
             }
             object IEnumerator.Current => throw new NotImplementedException();
             public bool MoveNext()
             {
-                if (Position < Animals.Length - 1)
+                if (Position < animals.Length - 1)
                 {
                     Position++;
                     return true;
