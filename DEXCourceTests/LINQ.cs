444797378ed104fb49fcf0ae80027e8b753a5485
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-14 19:12:44 +0200

    Add Cat class in LINQ,

---
+++ DEXCourceTests/LINQ.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace DEXCource
+{
+    class LINQ
+    {
+        
+    }
+    public class Cat
+    {
+        public Cat(int id, string name, int age, bool isMale, double weight)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.isMale = isMale;
+            this.weight = weight;
+        }
+        int id { get; }
+        string name { get; set; }
+        int age { get; set; }
+        bool isMale { get; }
+        double weight { get; set; }
+    }
+}
