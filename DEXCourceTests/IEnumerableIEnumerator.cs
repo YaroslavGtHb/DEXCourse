97fc89c85ad722f236c6029786b6f09688b911fa
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-14 16:07:44 +0200

    Add ZOO class to IEnumerableIEnumerator

---
+++ DEXCourceTests/IEnumerableIEnumerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DEXCource
+{
+    class IEnumerableIEnumerator
+    {
+    }
+
+    public class ZOO
+    {
+        string[] Animals = { "Lion", "Mokney" };
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new ZOOEnumerator(Animals);
+        }
+    }
+}
