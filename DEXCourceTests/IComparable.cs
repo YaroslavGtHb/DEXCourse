3971e622aa22eb7f12f274f8feffa4a016837c28
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-15 14:53:50 +0200

    Add GeometricFigure Class in IComparable

--- DEXCourceTests/IComparable.cs
+++ DEXCourceTests/IComparable.cs
@@ -7,4 +7,23 @@ namespace DEXCource
     class IComparable
     {
     }
+
+    class GeometricFigure : IComparable
+    {
+        public int Id;
+        public string Type { get; set; }
+        public int Square { get; set; }
+        public int CompareTo(object o)
+        {
+            GeometricFigure figure = o as GeometricFigure;
+            if (figure != null)
+            {
+                return Square.CompareTo(figure.Square);
+            }
+            else
+            {
+                throw new Exception("Невозможно сравнить два объекта");
+            }
+        }
+    }
 }
