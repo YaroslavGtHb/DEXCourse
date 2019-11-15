6e67206d60301a689978a3654be760f8782fc2ba
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-15 18:28:23 +0200

    Add casting to Casting.

---
+++ DEXCourceTests/Casting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEXCource
+{
+    class Casting
+    {
+        public void CastingTest()
+        {
+            var Pupkin = new Person("Иван", "Пупкин");
+            string PupkinString = "Петр Петров";
+            _ = (Person)PupkinString;
+            _ = PupkinString.GetType();
+        }
+    }
+    public class Person
+    {
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+        public static explicit operator Person(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
