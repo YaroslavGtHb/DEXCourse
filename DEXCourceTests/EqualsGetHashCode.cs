3f9025e793524c3a81f3d8621e947cd80194741a
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-16 22:23:48 +0200

    Add PersonInfo to EqualsGetHashCode.

---
+++ DEXCourceTests/EqualsGetHashCode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEXCource
+{
+    class EqualsGetHashCode
+    {
+    }
+    class PersonInfo
+    {
+        public PersonInfo(string FIO, string BirthDate, string BirthPlace, string PassportId)
+        {
+            this.FIO = FIO;
+            this.BirthDate = BirthDate;
+            this.BirthPlace = BirthPlace;
+            this.PassportId = PassportId;
+        }
+        string FIO { get; set; }
+        string BirthDate { get; set; }
+        string BirthPlace { get; set; }
+        string PassportId { get; set; }
+        public override int GetHashCode()
+        {
+            return Int32.Parse(FIO) + Int32.Parse(BirthDate) + Int32.Parse(BirthPlace) + Int32.Parse(PassportId);
+        }
+        public override bool Equals(object testedPerson)
+        {
+            PersonInfo person;
+            try
+            {
+                person = (PersonInfo)testedPerson;
+            }
+            catch(InvalidCastException)
+            {
+                return false;
+            }
+
+            if (FIO == person.FIO && BirthDate == person.BirthDate && BirthPlace == person.BirthPlace && PassportId == person.PassportId)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
