37b0370079711ad51e592ea4596839e4f5bedbdb
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-17 20:17:21 +0200

    Add tests to Events.

--- DEXCourceTests/Events.cs
+++ DEXCourceTests/Events.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,16 +7,48 @@ namespace DEXCource
 {
     class Events
     {
+        [Test]
+        public void EventsTest()
+        {
+            var quere = new Queue<int>();
+            var MaxWasCalled = false;
+            var NullWasCalled = false;
+            quere.MaxElementsNotify += delegate () { MaxWasCalled = true; };
+            quere.ZeroElementsNotify += delegate () { NullWasCalled = true; };
+            for (int i = 0; i < 28; i++)
+            {
+                quere.Enqueue(i);
+            }
+            for (int i = 0; i < 28; i++)
+            {
+                quere.Dequeue();
+            }
+            Assert.IsTrue(MaxWasCalled);
+            Assert.IsTrue(NullWasCalled);
+        }
     }
     public class Queue<T>
     {
         private List<T> _items = new List<T>();
         public int Count => _items.Count;
+        public int MaxElements = 8;
+        public  delegate void ElementsCountHandler();
+
+        public event ElementsCountHandler MaxElementsNotify;
+        public event ElementsCountHandler ZeroElementsNotify;
         public void Enqueue(T item)
         {
             if (item == null)
             {
                 throw new ArgumentNullException(nameof(item));
+            }
+            if(Count == MaxElements)
+            {
+                MaxElementsNotify();
+            }
+            if(Count == 0)
+            {
+                ZeroElementsNotify();
             }
             _items.Add(item);
         }
@@ -24,6 +56,14 @@ namespace DEXCource
         {
             var item = GetItem();
             _items.Remove(item);
+            if (Count == MaxElements)
+            {
+                MaxElementsNotify();
+            }
+            if (Count == 0)
+            {
+                ZeroElementsNotify();
+            }
             return item;
         }
         public T Peek()
