ae2290c6569d00ad0dafbfc320cfcdb28c360948
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-17 17:45:10 +0200

    Add UniqueCollection to GenericTypeGenericMember.

---
+++ DEXCourceTests/GenericTypeGenericMember.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+
+namespace DEXCource
+{
+    class GenericTypeGenericMember
+    {
+        [Test]
+        public void GenericTypeGenericMemberTest()
+        {
+            UniqueCollection<string> uniquecollection = new UniqueCollection<string>();
+            uniquecollection.AddUnique("Значение");
+            uniquecollection.AddUnique("Значение");
+            Assert.IsTrue(uniquecollection.Count == 1);
+        }
+    public class UniqueCollection<T> : List<T>
+    {
+        public void AddUnique(T item)
+        {
+                if(!this.Contains(item))
+                {
+                    this.Add(item);
+                }
+        }
+    }
+}
+}
