b52ae5ee3bc84fbd7ab288a37d0ce61617a496e4
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-17 19:46:07 +0200

    Add Queue realization in Events.

---
+++ DEXCourceTests/Events.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DEXCource
+{
+    class Events
+    {
+    }
+    public class Queue<T>
+    {
+        private List<T> _items = new List<T>();
+        public int Count => _items.Count;
+        public void Enqueue(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            _items.Add(item);
+        }
+        public T Dequeue()
+        {
+            var item = GetItem();
+            _items.Remove(item);
+            return item;
+        }
+        public T Peek()
+        {
+            var item = GetItem();
+            return item;
+        }
+
+        private T GetItem()
+        {
+            var item = _items.FirstOrDefault();
+            if (item == null)
+            {
+                throw new NullReferenceException("Очередь пуста. Нет элементов для получения.");
+            }
+            return item;
+        }
+    }
+}
+
