2912b4e1f6035c594a907da25111295bb83088eb
YaroslavGtHb <YrslvYaroslav@Gmail.Com>
2019-11-18 12:51:31 +0200

    Add extentions to Extentions.

--- DEXCourceTests/Extentions.cs
+++ DEXCourceTests/Extentions.cs
@@ -7,4 +7,27 @@ namespace DEXCource
     class Extentions
     {
     }
+    public class IntTimeSpanExtentions
+    {
+        public static TimeSpan ToMilliseconds(int number)
+        {
+            return new TimeSpan(0, 0, 0, 0, number);
+        }
+        public static TimeSpan ToSeconds(int number)
+        {
+            return new TimeSpan(0, 0, 0, number, 0);
+        }
+        public static TimeSpan ToMinutes(int number)
+        {
+            return new TimeSpan(0, 0, number, 0, 0);
+        }
+        public static TimeSpan ToHours(int number)
+        {
+            return new TimeSpan(0, number, 0, 0, 0);
+        }
+        public static TimeSpan ToDays(int number)
+        {
+            return new TimeSpan(number, 0, 0, 0, 0);
+        }
+    }
 }
