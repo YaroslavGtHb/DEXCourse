f39a1b6f46a8da3e9e0142e0e4b388948d00448a
HubForEducation <hubforeducation@yandex.ru>
2019-12-12 15:35:57 +0200

    Add DeepClone tests.

--- DEXCourceTests/DeepClone.cs
+++ DEXCourceTests/DeepClone.cs
@@ -4,11 +4,39 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using NUnit.Framework;
 
 namespace DEXCource
 {
     class DeepClone
     {
+        [Test]
+        public void DeepCloneTest()
+        {
+            var cloning = new ClonableClass(8, 88, true, "HelloWorld!");
+            var cloned = cloning.Clone();
+            Assert.AreEqual(cloning.intValue, cloned.intValue);
+        }
+    }
+    public class ClonableClass
+    {
+        public ClonableClass(int intValue, double DoubleValue, bool BoolValue, string StringValue)
+        {
+            this.intValue = intValue;
+            this.doubleValue = doubleValue;
+            this.boolValue = boolValue;
+            this.stringValue = stringValue;
+        }
+        public int intValue { get; set; }
+        public double doubleValue { get; set; }
+        public bool boolValue { get; set; }
+        public string stringValue { get; set; }
+
+        public ClonableClass Clone()
+        {
+            ClonableClass clonedClass = new ClonableClass(intValue, doubleValue, boolValue, stringValue);
+            return clonedClass;
+        }
     }
     public static class ObjectCopier
     {
@@ -16,7 +44,7 @@ namespace DEXCource
         {
             if (!typeof(T).IsSerializable)
             {
-                throw new ArgumentException("The type must be serializable.", "source");
+                throw new ArgumentException();
             }
             if (Object.ReferenceEquals(source, null))
             {
